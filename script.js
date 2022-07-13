9edf2858a4aaf23e90b9b8c2fd6c81b7fe9c92f1
Ramon Duque <ramonduke@gmail.com>
2022-07-13 12:48:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+for ( let i = 1; i <=10; i++){
+    console.log(i)
+}
+
+for ( let e = 10; e >0; e--){
+    console.log(e)
+}
+
+
+let a = 1
+while ( a < 10) {
+    a++;
+    console.log(a)
+}
+
+
+let t = 10
+while ( t > 1 ){
+t--;
+console.log(t)
+}
+
+
+let m = 1;
+do {
+    console.log(m);
+    m++;
+}
+while (m < 11)
+
+let w = 10;
+do {
+    console.log(w);
+    w--;
+}
+while (w > 0)
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+for (let number of numbers){
+    console.log(number);
+}
+
+
+
+
+
+
+
